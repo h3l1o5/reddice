0019aa25832703106e6e79d8ca3322905fdf5865
martysun25 <martysun25@gmail.com>
2017-08-21 08:28:26 +0800

    render main react component

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import path from 'path'
+import webpack from 'webpack'
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackConfig from '../webpack.config.dev.js'
 
 const app = express()
+
+app.use(webpackMiddleware(webpack(webpackConfig)))
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
